613cf596b8a277f87a6f27be5eeb51e9b2cb5c36
Yann <yann.wallis@gmail.com>
2024-12-06 09:28:17 +0800

    cleaned up code!

--- 5/part_2.py
+++ 5/part_2.py
@@ -1,7 +1,100 @@
 from icecream import ic 
 
+# Disabling icecream to clean up feed
+
+ic.disable()
+
+# Defining functions
+
+def list_passes(input_list: list[int], rules: list[tuple[int, int]]) -> bool:
+    """The goal of this function is simply to check if a input list passes the order rules.
+    It returns True if that is the case, and False otherwise. It does so by going through the input list iteratively, and checking if a rule exists
+    for any combination."""
+    
+    x_index: int = 0
+
+    correct_order: bool = True
+    
+    for x in input_list:
+        ic(x)
+        y_index: int = x_index + 1
+        for y in input_list[x_index + 1:]:
+            if (y, x) in rules:
+               ic(f'Rule for {y}, {x}')
+               ic(y_index)
+               if y_index < x_index:
+                    ic('Order is fine')
+
+               else:
+                    ic('Order not fine, breaking')
+                    correct_order = False
+                    break
+            if (x, y) in rules:
+                ic(f'Rule for {x}, {y}')
+                ic(y_index)
+                if x_index < y_index:
+                    ic('Order is fine')
+
+                else:
+                    ic('Order not fine, breaking')
+                    correct_order = False
+                    break
+            y_index += 1
+            
+        x_index += 1
+
+    if correct_order:
+        ic('List passes!')
+        return True
+    else:
+        return False
+        ('List fails!')
+
+
+def shuffle_list(input_list, rules):
+    
+    x_index = 0
+
+    for x in input_list:
+        ic(x)
+        
+        y_index = x_index + 1
+        
+        for y in input_list[x_index + 1:]:
+            
+            if (y, x) in rules:
+                ic(f'Rule for {y}, {x}')
+                ic(y_index)
+                if y_index < x_index:
+                    ic('Order is fine')
+
+                else:
+                    ic('Order not fine, breaking')
+                    input_list[y_index], input_list[x_index] = input_list[x_index], input_list[y_index]
+                    return input_list
+            if (x, y) in rules:
+                ic(f'Rule for {x}, {y}')
+                ic(y_index)
+                if x_index < y_index:
+                    ic('Order is fine')
+
+                else:
+                    ic('Order not fine, breaking')
+                    input_list[y_index], input_list[x_index] = input_list[x_index], input_list[y_index]
+                    return input_list
+            y_index += 1
+            
+        x_index += 1
+
+
+
+
+# Initialize lists to import from file
+
 rules = []
 numbers = []
+
+# Import values from files
 
 with open('input.txt', 'r') as file:
     
@@ -16,86 +109,14 @@ with open('input.txt', 'r') as file:
             line_n = line.strip().split(',')
             numbers.append(list(map(int, line_n)))
 
+# Display full value lists
+
 ic(rules)
 ic(numbers)
 
 valid_lists = []
 invalid_lists = []
-def shuffle_list(input_list, rules):
-    
-    x_index = 0
 
-    correct_order = True
-
-    for x in input_list:
-        # ic(x)
-        y_index = x_index + 1
-        for y in input_list[x_index + 1:]:
-            if (y, x) in rules:
-                # ic(f'Rule for {y}, {x}')
-                # ic(y_index)
-                if not y_index < x_index:
-                    # ic('Order is fine')
-
-                # else:
-                    # ic('Order not fine, breaking')
-                    correct_order = False
-                    input_list[y_index], input_list[x_index] = input_list[x_index], input_list[y_index]
-                    return input_list
-            if (x, y) in rules:
-                # ic(f'Rule for {x}, {y}')
-                # ic(y_index)
-                if not x_index < y_index:
-                    # ic('Order is fine')
-
-                # else:
-                    # ic('Order not fine, breaking')
-                    correct_order = False
-                    input_list[y_index], input_list[x_index] = input_list[x_index], input_list[y_index]
-                    return input_list
-            y_index += 1
-            
-        x_index += 1
-def list_passes(input_list, rules):
-    
-    x_index = 0
-
-    correct_order = True
-
-    for x in input_list:
-        # ic(x)
-        y_index = x_index + 1
-        for y in input_list[x_index + 1:]:
-            if (y, x) in rules:
-               # ic(f'Rule for {y}, {x}')
-               # ic(y_index)
-                if not y_index < x_index:
-                    # ic('Order is fine')
-
-                # else:
-                    # ic('Order not fine, breaking')
-                    correct_order = False
-                    break
-            if (x, y) in rules:
-                # ic(f'Rule for {x}, {y}')
-                # ic(y_index)
-                if not x_index < y_index:
-                    # ic('Order is fine')
-
-                # else:
-                    # ic('Order not fine, breaking')
-                    correct_order = False
-                    break
-            y_index += 1
-            
-        x_index += 1
-
-    if correct_order:
-        # ic('List passes!')
-        return True
-    else:
-        return False
-        # ic('List fails!')
 
 for _ in numbers:
     if list_passes(_, rules):
@@ -112,7 +133,7 @@ for _ in invalid_lists:
     while not list_passes(_, rules):
         _ = shuffle_list(_, rules)
     corrected_lists.append(_)
-    ic(f'Found {bf_index} solutions of {len(invalid_lists)}')
+    print(f'Found {bf_index} solutions of {len(invalid_lists)}')
     bf_index += 1
 
 
@@ -123,6 +144,8 @@ for _ in corrected_lists:
     middle_index = len(_) // 2
     middles.append(_[middle_index])
     sum_of_middles += _[middle_index]
+
+ic.enable()
 
 ic(middles)
 ic(sum_of_middles)
