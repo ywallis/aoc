a0882e207bca61c808a674330b1462de8796d1a2
Yann <yann.wallis@gmail.com>
2025-01-27 14:50:23 +0700

    Saved and cleaned python solution for day 2

--- 2024/python/day2.py
+++ 2024/python/day2.py
@@ -1,33 +1,58 @@
-with open('example.txt', 'r') as file:
+from copy import copy
+
+def line_safe(list:list[int]) -> bool:
+
+    if list == sorted(list) or list == sorted(list, reverse=True):
+
+        for item in range(len(list)):
+
+            # Item 0 has no preceding
+            
+            if item == 0:
+                continue
+            else:
+                current = int(list[item])
+                preceding = int(list[item-1])
+            
+            # Checking if difference from current to preceding is within allowed range.
+            
+            if not 1 <= abs(current - preceding) <= 3:
+                return False
+        
+        return True
+
+
+    else:
+        return False
+
+def dampener(list:list[int]):
+
+    for index in range(len(list)):
+        list_c = copy(list)
+        del(list_c[index])
+        if line_safe(list_c):
+            return True
+    
+    return False
+
+
+with open('../inputs/2_input.txt', 'r') as file:
     
     lines = file.readlines()
     
     safe_lines: int = 0
+    safe_with_dampener: int = 0
     
     for line in lines:
         
-        line_clean: list = list(line.strip().split())
-        print(line_clean)
-        
-        if line_clean == sorted(line_clean) or line_clean == sorted(line_clean, reverse=True):
-            print('Direction is valid! Continuing')
+        line_str: list[str] = list(line.strip().split())
+        line_clean: list[int] = list(map(int, line_str))
+        if line_safe(line_clean):
+            safe_lines += 1
+                    
+        else: 
 
-            for item in range(len(line_clean)):
-                if item == 0:
-                    continue
-                else:
-                    current = int(line_clean[item])
-                    preceding = int(line_clean[item-1])
-                
-                if current == preceding:
-                    print('Repeating value')
-                    break
+            if dampener(line_clean):
+                safe_with_dampener += 1
     
-                elif abs(current - preceding) > 3:
-                    print('Difference too high')
-                    break
-        safe_lines += 1
-    
-    print(safe_lines)
-
-
+    print(f'There are {safe_lines} without the dampener, {safe_lines + safe_with_dampener} with it.')
