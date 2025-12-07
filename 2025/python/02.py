cf74bbd0dfcff12aea486f897e82542cc23a4c7e
yann <yann.wallis@gmail.com>
2025-12-07 11:18:07 +0100

    added part 2 day 2 2025 python

--- 2025/python/02.py
+++ 2025/python/02.py
@@ -38,10 +38,31 @@ def part_1(id_blocks: ranges) -> int:
     return count
 
 
+def part_2(id_blocks: ranges) -> int:
+    invalid = set()
+
+    for n in range(1, 100_000):
+        r = 2
+
+        d = n
+
+        while int(str(n) * r) < 10_000_000_000:
+            d = int(str(n) * r)
+
+            for block in id_blocks:
+                range_min = block[0]
+                range_max = block[1]
+                if range_min <= d <= range_max:
+                    invalid.add(d)
+            r += 1
+
+    return sum(invalid)
+
+
 def main():
     line = load_example("02", False)
     id_blocks: ranges = get_ranges(line)
-    print(part_1(id_blocks))
+    print(part_2(id_blocks))
 
 
 if __name__ == "__main__":
