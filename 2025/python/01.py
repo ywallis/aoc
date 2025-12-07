c026e76359a231668dd2d919e64cbfd2a9096880
yann <yann.wallis@gmail.com>
2025-12-07 09:46:24 +0100

    added part 2 of day 1 2025 python

--- 2025/python/01.py
+++ 2025/python/01.py
@@ -1,3 +1,8 @@
+from typing import TypeAlias
+
+rotations: TypeAlias = list[tuple[str, int]]
+
+
 def load_example(day: str, example: bool = True):
     file_path = (
         f"../inputs/{day}_example.txt" if example else f"../inputs/{day}_input.txt"
@@ -6,14 +11,13 @@ def load_example(day: str, example: bool = True):
         lines = file.readlines()
         cleaned_output = []
         for line in lines:
-            line = line.replace("\n", "")
-            line.strip()
+            line = line.strip()
             cleaned_output.append(line)
 
     return cleaned_output
 
 
-def split_rotations(rotations: list[str]) -> list[tuple[str, int]]:
+def split_rotations(rotations: list[str]) -> rotations:
     output = []
 
     for i, rotation in enumerate(rotations):
@@ -21,11 +25,7 @@ def split_rotations(rotations: list[str]) -> list[tuple[str, int]]:
     return output
 
 
-def main():
-    input = load_example("01", False)
-
-    rotations = split_rotations(input)
-
+def find_part_1(rotations: rotations) -> int:
     dial = 50
     count = 0
 
@@ -35,12 +35,41 @@ def main():
         if rotation[0] == "L":
             dial -= rotation[1]
         dial = dial % 100
-        if dial < 0:
-            dial = 100 + dial
         if dial == 0:
             count += 1
 
-    print(count)
+    return count
+
+
+def find_part_2(rotations: rotations) -> int:
+    virtual_dial = 50
+    count = 0
+
+    for direction, amount in rotations:
+        prev_dial = virtual_dial
+
+        if direction == "R":
+            virtual_dial += amount
+            count += (virtual_dial // 100) - (prev_dial // 100)
+
+        elif direction == "L":
+            virtual_dial -= amount
+            count += ((prev_dial - 1) // 100) - ((virtual_dial - 1) // 100)
+
+        # print(virtual_dial, count, virtual_dial % 100)
+
+    return count
+
+
+def main():
+    input = load_example("01", False)
+
+    rotations = split_rotations(input)
+
+    # part_1 = find_part_1(rotations)
+    # print(part_1)
+    part_2 = find_part_2(rotations)
+    print(part_2)
 
 
 if __name__ == "__main__":
