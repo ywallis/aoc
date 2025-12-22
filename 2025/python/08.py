e0ace6ca86f82b2d94ec59db3801330311e4e1d6
Yann Wallis <yann.wallis@gmail.com>
2025-12-22 12:23:20 +0100

    correct part 1 and 2 day 8 2025 python

--- 2025/python/08.py
+++ 2025/python/08.py
@@ -2,6 +2,27 @@ import math
 from itertools import combinations
 
 
+class UnionFind:
+    def __init__(self, size: int) -> None:
+        self.parent = list(range(size))
+        self.size = [1] * size
+        self.sets = size
+
+    def find(self, i: int) -> int:
+        if self.parent[i] == i:
+            return i
+        return self.find(self.parent[i])
+
+    def unite(self, i: int, j: int):
+        irep = self.find(i)
+        jrep = self.find(j)
+        if irep != jrep:
+            self.parent[irep] = jrep
+            self.size[jrep] += self.size[irep]
+            self.size[irep] = 0
+            self.sets -= 1
+
+
 def load_example(day: str, example: bool = True):
     file_path = (
         f"../inputs/{day}_example.txt" if example else f"../inputs/{day}_input.txt"
@@ -21,36 +42,42 @@ def euclidian_distance(x: tuple[str, ...], y: tuple[str, ...]) -> float:
 
 
 def part_1(boxes: list[tuple[str, ...]], limit: int) -> int:
-    collections: list[set[tuple[str, ...]]] = []
-    combos = combinations(boxes, 2)
-    distances = [(euclidian_distance(x, y), x, y) for x, y in combos]
+    combos = combinations(range(len(boxes)), 2)
+    distances = [(euclidian_distance(boxes[x], boxes[y]), x, y) for x, y in combos]
     distances.sort(key=lambda x: x[0])
 
-    for i in range(limit + 1):
-        box_x = distances[i][1]
-        box_y = distances[i][2]
+    uf = UnionFind(len(boxes))
+    for i in range(limit):
+        uf.unite(distances[i][1], distances[i][2])
+    t3 = sorted(uf.size, reverse=True)[:3]
 
-        added = False
-        for collection in collections:
-            if box_x in collection or box_y in collection:
-                collection.add(box_x)
-                collection.add(box_y)
-                added = True
-                break
-        if not added:
-            collections.append(set([box_x, box_y]))
+    return math.prod(t3)
 
-    collections.sort(key=lambda x: len(x), reverse=True)
-    top3l = [len(x) for x in collections[:3]]
 
-    return math.prod(top3l)
+def part_2(boxes: list[tuple[str, ...]]) -> int:
+    combos = combinations(range(len(boxes)), 2)
+    distances = [(euclidian_distance(boxes[x], boxes[y]), x, y) for x, y in combos]
+    distances.sort(key=lambda x: x[0])
+
+    uf = UnionFind(len(boxes))
+    i = 0
+    while uf.sets > 1:
+        uf.unite(distances[i][1], distances[i][2])
+        i += 1
+
+    a, b = boxes[distances[i - 1][1]], boxes[distances[i - 1][2]]
+
+    return int(a[0]) * int(b[0])
 
 
 def main():
-    input = load_example("08", False)
+    input = load_example(
+        "08",
+    )
 
     LIMIT = 1000
     print(part_1(input, LIMIT))
+    print(part_2(input))
 
 
 if __name__ == "__main__":
